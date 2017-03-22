79aec0b73520d12e1dea4098c0e9e4f81e69e255
Wintory <wintory@users.noreply.github.com>
2017-03-22 08:37:57 +0700

    Update Subject.java

--- src/timetable/model/Subject.java
+++ src/timetable/model/Subject.java
@@ -119,6 +119,7 @@ public class Subject {
         } catch (Exception e) {
             System.out.println(e);
         }
+        con.close();
         return allsub;
     }
     
@@ -149,6 +150,7 @@ public class Subject {
         } catch (Exception e) {
             System.out.println(e);
         }
+        con.close();
         return allsub;
     }
     
@@ -170,6 +172,7 @@ public class Subject {
         } catch (Exception e) {
             System.out.println(e);
         }
+        con.close();
         return s;
     }
     
@@ -199,6 +202,7 @@ public class Subject {
         } catch (Exception e) {
             System.out.println(e);
         }
+          con.close();
         return allsub;
     }
     
@@ -227,6 +231,7 @@ public class Subject {
         } catch (Exception e) {
             System.out.println(e);
         }
+        con.close();
         return allsub;
     }
     
