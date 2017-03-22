eb2952e9599006461ba93d6917c4be8aa9f49567
Wintory <willyvongga@hotmail.com>
2017-03-22 09:26:46 +0700

    slove db connection

--- src/timetable/connection/ConnectionBuilder.java
+++ src/timetable/connection/ConnectionBuilder.java
@@ -6,14 +6,19 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class ConnectionBuilder{
+public class ConnectionBuilder {
+
+    public static Connection getConnection() throws SQLException {
+
+        Connection con = DriverManager.getConnection("jdbc:mysql://ap-cdbr-azure-southeast-b.cloudapp.net:3306/preregis?zeroDateTimeBehavior=convertToNull", "b271179b96ae42", "08e9afdb");
+        System.out.println("db connected");
+        return con;
+
+    }
     
-  public static Connection getConnection() throws SQLException
-  {
- 
-Connection con = DriverManager.getConnection("jdbc:mysql://ap-cdbr-azure-southeast-b.cloudapp.net:3306/preregis?zeroDateTimeBehavior=convertToNull", "b271179b96ae42", "08e9afdb");
-      System.out.println("db connected");
-      return con;
-   
-}
+  /*  public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection conn = DriverManager.getConnection("jdbc:mysql://10.4.56.10:3306/mysql?zeroDateTimeBehavior=convertToNull", "new", "new");
+        System.out.println("Connected");
+    }*/
 }
